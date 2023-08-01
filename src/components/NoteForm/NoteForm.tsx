911219e71332be975cc7710031407a6c61805fd1
v_code <12273445-v_code@users.noreply.gitlab.com>
2023-08-01 13:56:16 +0300

    form processing

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -1,32 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 const NoteForm: React.FC = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        category: 'Task',
+        content: '',
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log('Form data:', formData);
+    };
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <FormGroup>
-                <Label for="nameText">Name</Label>
+                <Label for="name">Name</Label>
                 <Input
-                    id="nameText"
-                    name="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
                     placeholder="...enter note title"
+                    type="text"
+                    onChange={handleChange}
                 />
             </FormGroup>
             <FormGroup>
-                <Label for="categorySelect">Category</Label>
-                <Input id="categorySelect" name="select" type="select">
+                <Label for="category">Category</Label>
+                <Input
+                    id="category"
+                    name="category"
+                    value={formData.category}
+                    type="select"
+                    onChange={handleChange}
+                >
                     <option>Task</option>
                     <option>Random Thought</option>
                     <option>Idea</option>
                 </Input>
             </FormGroup>
             <FormGroup>
-                <Label for="contentText">Content</Label>
+                <Label for="content">Content</Label>
                 <Input
-                    id="contentText"
-                    name="text"
+                    id="content"
+                    name="content"
+                    value={formData.content}
                     placeholder="...write about this"
                     type="textarea"
+                    onChange={handleChange}
                 />
             </FormGroup>
             <Button>Submit</Button>
