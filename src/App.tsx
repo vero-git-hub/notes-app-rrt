14dfce613b1dd0d5085cf37878f6b0067dea14ca
v_code <12273445-v_code@users.noreply.gitlab.com>
2023-08-02 10:11:45 +0300

    summary table

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import NoteList from './components/NoteList';
 import ArchiveList from "./components/ArchiveList";
+import SummaryTable from "./components/SummaryTable";
 
 const App: React.FC = () => {
     return (
         <div>
             <NoteList />
+            <SummaryTable />
             <ArchiveList />
         </div>
     );
--- src/components/SummaryTable/SummaryTable.tsx
+++ src/components/SummaryTable/SummaryTable.tsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import TableTemplate from '../TableTemplate';
+import {AppState} from "../../redux/types";
+import {useSelector} from "react-redux";
 
 const SummaryTable: React.FC = () => {
+    const notes = useSelector((state: AppState) => state.notes);
+    const archivedNotes = useSelector((state: AppState) => state.archivedNotes);
+
+    const categoryCounts: { [category: string]: number } = {};
+    notes.forEach((note) => {
+        if (categoryCounts[note.category]) {
+            categoryCounts[note.category]++;
+        } else {
+            categoryCounts[note.category] = 1;
+        }
+    });
+
+    const archivedCategoryCounts: { [category: string]: number } = {};
+    archivedNotes.forEach((note) => {
+        if (archivedCategoryCounts[note.category]) {
+            archivedCategoryCounts[note.category]++;
+        } else {
+            archivedCategoryCounts[note.category] = 1;
+        }
+    });
+
     const summaries = [
         {
-            noteCategory: 'Row 1, Column 1',
-            active: 'Row 1, Column 2',
-            archived: 'Row 1, Column 3',
+            noteCategory: 'Task',
+            active: categoryCounts['Task'] || 0,
+            archived: archivedCategoryCounts['Task'] || 0,
         },
         {
-            noteCategory: 'Row 2, Column 1',
-            active: 'Row 2, Column 2',
-            archived: 'Row 2, Column 3',
+            noteCategory: 'Random Thought',
+            active: categoryCounts['Random Thought'] || 0,
+            archived: archivedCategoryCounts['Random Thought'] || 0,
+        },
+        {
+            noteCategory: 'Idea',
+            active: categoryCounts['Idea'] || 0,
+            archived: archivedCategoryCounts['Idea'] || 0,
         },
     ];
 
@@ -21,7 +49,10 @@ const SummaryTable: React.FC = () => {
         { label: 'Archived', field: 'archived' },
     ];
 
-    const data = summaries;
+    const data = summaries.map((summary, index) => ({
+        ...summary,
+        id: index + 1,
+    }));
 
     return <TableTemplate columns={columns} data={data} />;
 };
